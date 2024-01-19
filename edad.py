2c29487f0e37b74bf8cbffebc2a6d939c1aa2ad6
Nay Pino <nakavapi.30@gmail.com>
2024-01-18 20:52:25 -0500

    Ingeniera nay hace cambios en edad.py

--- edad.py
+++ edad.py
@@ -2,5 +2,7 @@ nombre = input("Por favor digite el nombre: ")
 edad = int(input("Escriba su edad, por favor: "))
 if edad >= 18:
     print(f"{nombre}, Eres mayor de edad")
+elif edad >= 60 and edad <=99:
+    print(f"{nombre}, eres de la tercera edad")
 else:
     print(f"{nombre}, Eres menor de edad")
